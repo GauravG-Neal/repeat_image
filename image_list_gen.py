2a0b6c574a58105c67cfea9d3779840e0a490bcc
GauravG-Neal <60956816+GauravG-Neal@users.noreply.github.com>
2020-04-16 10:05:45 +0530

    Delete image_list_gen.py

--- image_list_gen.py
+++
@@ -1,7 +0,0 @@
-import glob
-all_images = (glob.glob("C:\walgreens_images\Code\*.jpg"))
-
-with open('image_list.txt', 'w') as f:
-    for item in all_images:
-        f.write("%s\n" % item)
-        
